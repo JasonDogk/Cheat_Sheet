ab3eebcedee1c242b0330071b1ed423975d5d08b
Safirenios <forgetable@hotmail.gr>
2020-06-22 17:44:21 +0300

    First commit for JAVA builder design pattern implementation

---
+++ JAVA_BuilderDesignPattern/src/JavaBuilderDesignPatternMainClass.java
@@ -0,0 +1,17 @@
+// This is a typical example of a java Builder Design Pattern.
+// All attributes in User.class have to be final in order not to be able to change after the first initialization of the object.
+// Setters are also not allowed in order to make the object immutable.
+// If you want to have attributes in User.class that are optional and not always required, you can make them by not using the final word in the UserBuilder class
+
+public class JavaBuilderDesignPatternMainClass {
+
+	public static void main(String[] args) {
+
+		User user1 = new User.UserBuilder("John", "Smith").age(39).phone("+306972463857")
+				.address("56 Burmingham Street").build();
+
+		System.out.println(user1);
+
+	}
+
+}
